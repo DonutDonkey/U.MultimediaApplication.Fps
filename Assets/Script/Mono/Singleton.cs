dbdaf93806d2f7e49d326024dcf5501880f92474
KGM <34782950+DonutDonkey@users.noreply.github.com>
2021-10-11 20:31:32 +0200

    11.10.2021

---
+++ Assets/Script/Mono/Singleton.cs
@@ -0,0 +1,17 @@
+using UnityEngine;
+
+namespace Script.Mono {
+    public class Singleton<T> : MonoBehaviour where T : Component {
+        public static T Instance { get; private set; }
+
+        private void Awake() {
+            if (Instance == null) {
+                Debug.Log(GetType().Name + "- Instance Created");
+                Instance = this as T;
+            } else {
+                Debug.Log(GetType().Name + "- Instance Destroyed");
+                Destroy(gameObject);
+            }
+        }
+    }
+}
