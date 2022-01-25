ccd393f9444b8bcf3abb9f72eea648c589eb678b
Konrad <34782950+DonutDonkey@users.noreply.github.com>
2022-01-25 13:07:19 +0100

    exit

---
+++ Assets/Script/Mono/Exit.cs
@@ -0,0 +1,7 @@
+using UnityEngine;
+
+namespace Script.Mono {
+    public class Exit : MonoBehaviour {
+        public void ExitApp() => Application.Quit();
+    }
+}
--- Assets/Script/Mono/Pickup.cs
+++ Assets/Script/Mono/Pickup.cs
@@ -9,7 +9,10 @@ namespace Script.Mono {
         public int amount;
 
         public void Invoke() {
-            if (ref_val.runtime_value + amount > 100) return;
+            if (ref_val.runtime_value > 100) return;
+            
+            if (ref_val.runtime_value + amount > 100) 
+                amount = 100 - ref_val.runtime_value;
             
             e_pickup_event.Invoke(amount);
             gameObject.SetActive(false);
